163add4d58948b86e4ffc8d4a89f9ecce82243b3
Jahanda97 <jahangirjn@code.edu.az>
2022-05-04 13:27:02 +0400

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Market
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product sweetProduct = new Product("Karamel", 1.02, 17);
+            Milk palMilk = new Milk("Pal",2.53,25,0.53,2.3);
+
+            sweetProduct.Sell(3);
+
+            palMilk.Sell(12);
+
+        }
+    }
+}
